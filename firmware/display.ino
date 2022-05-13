d4d3330f8afeb2dee6d0074719feae9899fe4470
bosko <bosko.petreski@gmail.com>
2022-05-14 00:22:43 +0200

    firmware

---
+++ firmware/display.ino
@@ -0,0 +1,3 @@
+void displayInfo() {
+  
+}
--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -1,146 +1,166 @@
 #include <Wire.h>
-#include <Servo.h>
-#include <PPMReader.h> // https://github.com/dimag0g/PPM-reader
-#include <ArduPID.h> // https://github.com/PowerBroker2/ArduPID
-#include <TinyGPSPlus.h> // https://github.com/mikalhart/TinyGPSPlus
-#include <Adafruit_MPU6050.h> // https://github.com/adafruit/Adafruit_MPU6050
-#include <Adafruit_Sensor.h>
-#include <NeoSWSerial.h> // https://github.com/SlashDevin/NeoSWSerial
-//#include <ServoTimer2.h> // alternate
+#include <SPI.h>
+#include <NeoHWSerial.h>
+#include <NeoSWSerial.h>
+//#include <Adafruit_GFX.h>
+//#include <Adafruit_SH110X.h>
+//#include <PPMReader.h> // https://github.com/dimag0g/PPM-reader
+//#include <ArduPID.h>           // https://github.com/PowerBroker2/ArduPID
+#include <TinyGPSPlus.h>       // https://github.com/mikalhart/TinyGPSPlus
+//#include <Adafruit_MPU6050.h>  // https://github.com/adafruit/Adafruit_MPU6050
+//#include <Adafruit_Sensor.h>
+//#include <ServoTimer2.h>  // alternate
+
+
+//#define i2c_Address 0x3c
+//#define SCREEN_WIDTH 128
+//#define SCREEN_HEIGHT 64
+//#define OLED_RESET -1
+//Adafruit_SH1106G displayOled = Adafruit_SH1106G(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 // PPM
 const int PPMChannels = 5; // Max channels
-int PPMinput = 3; // Must be interrupt
-PPMReader ppm(PPMinput, PPMChannels);
-unsigned ch1 = 0;
-unsigned ch2 = 0;
-unsigned ch3 = 0;
-unsigned ch4 = 0;
-unsigned mode = 0;
+const int PPMinput = 3; // Must be interrupt
+//PPMReader ppm(PPMinput, PPMChannels);
+unsigned int ch1 = 0;
+unsigned int ch2 = 0;
+unsigned int ch3 = 0;
+unsigned int ch4 = 0;
+unsigned int mode = 0;
 
 // PIDS ROLL
-ArduPID pidRoll;
-double inputRoll;
-double outputRoll;
-double setpointRoll = 0;
-double pRoll = 10;
-double iRoll = 0.01;
-double dRoll = 5;
+//ArduPID pidRoll;
+//double inputRoll;
+//double outputRoll;
+//double setpointRoll = 0.0;
+//const double pRoll = 10.0;
+//const double iRoll = 0.01;
+//const double dRoll = 5.0;
 
 // PIDS PITCH
-ArduPID pidPitch;
-double inputPitch;
-double outputPitch;
-double setpointPitch = 0;
-double pPitch = 10;
-double iPitch = 0.01;
-double dPitch = 5;
+//ArduPID pidPitch;
+//double inputPitch;
+//double outputPitch;
+//double setpointPitch = 0.0;
+//const double pPitch = 10.0;
+//const double iPitch = 0.01;
+//const double dPitch = 5.0;
 
 // SIGNAL LEDS
-const int LED_PWR = 2;
-const int LED_GPS = 4;
-const int LED_MODE = 5;
+//const int LED_PWR = 2;
+//const int LED_GPS = 4;
+//const int LED_MODE = 5;
 
 // SERVOS
-Servo servoLeft;
-Servo servoRight;
-Servo servoTail;
-const int LEFT_WING = 9;
-const int RIGHT_WING = 10;
-const int TAIL_WING = 6;
+//ServoTimer2 servoLeft;
+//ServoTimer2 servoRight;
+//ServoTimer2 servoTail;
+//const int LEFT_WING = 9;
+//const int RIGHT_WING = 10;
+//const int TAIL_WING = 6;
 
 // VOLTAGE READ
-float voltage = 0.0;
-float R1 = 100000.0;
-float R2 = 10000.0;
-const int VOLTAGE_READ_PIN = A2;
+//float voltage = 0.0;
+//const float R1 = 100000.0;
+//const float R2 = 10000.0;
+//const int VOLTAGE_READ_PIN = A2;
 
 // MPU6050
-Adafruit_MPU6050 mpu;
-sensors_event_t a, g, temp;
+//Adafruit_MPU6050 mpu;
+//sensors_event_t a, g, temp;
 
 // GPS
-TinyGPSPlus gps;
-static const int RXPin = 7;
-static const int TXPin = 8;
-NeoSWSerial gpsSerial(RXPin, TXPin);
+volatile TinyGPSPlus gps;
+double home_lat = 0.0;
+double home_lng = 0.0;
 
-long loop_timer;
+NeoSWSerial debug(4, 3);
+
+
+//long loop_timer;
 
 void setup() {
-  Serial.begin(115200);
-  gpsSerial.begin(9600);
+  
+  NeoSerial.attachInterrupt(readGPS);
+  NeoSerial.begin(9600);
+  
+  debug.begin(9600);
+  debug.println("------- STARTED -------");
+  
+//  pinMode(13, OUTPUT);
+  //  pinMode(LED_PWR, OUTPUT);
+  //  pinMode(LED_MODE, OUTPUT);
+  //  pinMode(LED_GPS, OUTPUT);
 
-  pinMode(LED_PWR, OUTPUT);
-  pinMode(LED_MODE, OUTPUT);
-  pinMode(LED_GPS, OUTPUT);
 
-  servoLeft.attach(LEFT_WING);
-  servoRight.attach(RIGHT_WING);
-  servoTail.attach(TAIL_WING);
+
+  //GPS Setup
+
+  //  servoLeft.attach(LEFT_WING);
+  //  servoRight.attach(RIGHT_WING);
+  //  servoTail.attach(TAIL_WING);
 
   // MPU6050 setup
-  if (!mpu.begin()) {
-    MPU_missing();
-  }
-  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
-  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
-  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  //  mpu.begin();
+  //  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  //  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  //  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
 
   // PPM setup
-  ppm.channelValueMaxError = 100; // trashhold PPM for min / max
+  //  ppm.channelValueMaxError = 100; // trashhold PPM for min / max
 
   // PID setup
-  pidRoll.begin(&inputRoll, &outputRoll, &setpointRoll, pRoll, iRoll, dRoll);
-  pidRoll.setOutputLimits(-45, 45);
-  pidRoll.setWindUpLimits(-6, 6);
-  pidRoll.start();
-
-  // PID setup
-  pidPitch.begin(&inputPitch, &outputPitch, &setpointPitch, pPitch, iPitch, dPitch);
-  pidPitch.setOutputLimits(-45, 45);
-  pidPitch.setWindUpLimits(-6, 6);
-  pidPitch.start();
+  //  pidRoll.begin(&inputRoll, &outputRoll, &setpointRoll, pRoll, iRoll, dRoll);
+  //  pidRoll.setOutputLimits(-45, 45);
+  //  pidRoll.setWindUpLimits(-6, 6);
+  //  pidRoll.start();
+  //
+  //  // PID setup
+  //  pidPitch.begin(&inputPitch, &outputPitch, &setpointPitch, pPitch, iPitch, dPitch);
+  //  pidPitch.setOutputLimits(-45, 45);
+  //  pidPitch.setWindUpLimits(-6, 6);
+  //  pidPitch.start();
 
   //Voltage Pin
-  pinMode(VOLTAGE_READ_PIN, INPUT);
+  //  pinMode(VOLTAGE_READ_PIN, INPUT);
 
-  loop_timer = micros();
+//  loop_timer = micros();
 
-  digitalWrite(LED_PWR, HIGH);
+//  digitalWrite(13, HIGH);
+//  delay(250);
+//  displayOled.begin(i2c_Address, true);
+//  displayOled.setTextSize(1);
+//  displayOled.setTextColor(SH110X_WHITE);
+//  displayOled.setCursor(0, 0);
+//  displayOled.println("NoahFC started");
+//  displayOled.display();
+  
+//  delay(2000);
+//  digitalWrite(13, LOW);
 }
 
 void loop() {
-  readVoltage();
-  readPPM();
-  leds();
-  pidRoll.compute();
-  pidPitch.compute();
-  mpu.getEvent(&a, &g, &temp);
+  displayInfo();
+  //  readVoltage();
+  //  readPPM();
+  //  leds();
+  //  setHomePosition();
+  //  pidRoll.compute();
+  //  pidPitch.compute();
+  //  mpu.getEvent(&a, &g, &temp);
 
   // Servos
-  inputRoll = a.acceleration.y;
-  inputPitch = a.acceleration.x;
-  servoLeft.write(outputRoll + 90);
-  servoRight.write(90 - outputRoll);
-  servoTail.write(outputPitch + 90);
+  //  inputRoll = a.acceleration.y;
+  //  int mapRoll = map(outputRoll, -45, 45, 2000, 1000);
+  //  servoLeft.write(mapRoll);
+  //  servoRight.write(mapRoll);
 
-  if (gpsSerial.available()) {
-    char c = gpsSerial.read();
-    gps.encode(c);
-    digitalWrite(LED_GPS, HIGH);
-  }
+  //  inputPitch = a.acceleration.x;
+  //  int mapPitch = map(outputPitch, -45, 45, 2000, 1000);
+  //  servoTail.write(mapPitch);
 
-//  Serial.print("Valid: ");
-//  Serial.print(gps.location.isValid());
-//  Serial.print("  Sats: ");
-//  Serial.print(gps.satellites.value());
-//  Serial.print("  Lat: ");
-//  Serial.print(gps.location.lat());
-//  Serial.print("  Lon: ");
-//  Serial.println(gps.location.lng());
-
-  digitalWrite(LED_GPS, gps.location.isValid());
-  while (micros() - loop_timer < 4000); //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
-  loop_timer = micros();
+  // 20000 - 50hz
+  // 4000 - 250Hz
+//  while (micros() - loop_timer < 4000);  //Wait until the loop_timer reaches 4000us (250Hz) before starting the next loop
+//  loop_timer = micros();
 }
