3799570467284cd469b079477926e228e9fb7f4a
bosko <bosko.petreski@gmail.com>
2022-06-30 16:36:33 +0200

    add leds funcs

---
+++ firmware/led.ino
@@ -0,0 +1,22 @@
+void blinkLeds() {
+  digitalWrite(PIN_LED_MODE, HIGH);
+  delay(200);
+  digitalWrite(PIN_LED_MODE, LOW);
+  digitalWrite(PIN_LED_GPS, HIGH);
+  delay(200);
+  digitalWrite(PIN_LED_GPS, LOW);
+  digitalWrite(PIN_LED_POWER, HIGH);
+  delay(200);
+}
+void ledManualMode() {
+  digitalWrite(PIN_LED_MODE, LOW);
+}
+void ledAngleMode() {
+  digitalWrite(PIN_LED_MODE, HIGH);
+}
+void ledGpsReceive() {
+  digitalWrite(PIN_LED_GPS, HIGH);
+}
+void ledGpsIdle() {
+  digitalWrite(PIN_LED_GPS, LOW);
+}
