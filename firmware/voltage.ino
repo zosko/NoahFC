d21cf1559379b2ff024940b619528dad142dbf78
bosko <bosko.petreski@gmail.com>
2022-06-30 16:36:49 +0200

    add correction in voltage read

--- firmware/voltage.ino
+++ firmware/voltage.ino
@@ -1,5 +1,6 @@
 void readVoltage() {
   int value = analogRead(PIN_VOLTAGE);
   float vout = (value * 5.0) / 1024.0;
-  voltage = vout / (R2 / (R1 + R2));
+  float correction = 0.15; // used to make correction on voltage display
+  voltage = vout / (R2 / (R1 + R2)) + correction;
 }
