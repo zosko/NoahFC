96cf015c54686d2c181bb9bf2be95af5b825cc29
bosko <bosko.petreski@gmail.com>
2022-06-30 16:37:02 +0200

    improve navigation

--- firmware/gps.ino
+++ firmware/gps.ino
@@ -2,10 +2,12 @@ static void GPSisr(uint8_t c) {
   gps.handle(c);
 }
 void readGPS() {
+  ledGpsIdle();
   if (gps.available()) {
+    ledGpsReceive();
     fix = gps.read();
-
-    if (fix.satellites > 4 && homeLocation.lat() == 0 && homeLocation.lon() == 0) {
+    
+    if (fix.satellites > 5 && homeLocation.lat() == 0 && homeLocation.lon() == 0) {
       homeLocation = fix.location;
     }
 
@@ -13,26 +15,26 @@ void readGPS() {
     float courseToDestination = fix.location.BearingToDegrees(homeLocation);
     courseChangeNeeded = (int)(360 + courseToDestination - fix.heading()) % 360;
 
-    if (distanceToHome < 30) {
-      // FLIGHT CIRCLE OVER LOCATION
+    if (distanceToHome < 30) { // FLIGHT CIRCLE OVER LOCATION
+      nav_dir = '@';
     } else {
-      if (courseChangeNeeded >= 345 || courseChangeNeeded < 15) {
-        // FLIGHT FORWARD
+      if (courseChangeNeeded >= 345 || courseChangeNeeded < 15) { // FLIGHT FORWARD
+        nav_dir = '^';
       }
-      else if (courseChangeNeeded >= 315 && courseChangeNeeded < 345) {
-        // MOVE LITTLE LEFT
+      else if (courseChangeNeeded >= 315 && courseChangeNeeded < 345) { // MOVE LITTLE LEFT
+        nav_dir = '{';
       }
-      else if (courseChangeNeeded >= 15 && courseChangeNeeded < 45) {
-        // MOVE LITTLE RIGHT
+      else if (courseChangeNeeded >= 15 && courseChangeNeeded < 45) { // MOVE LITTLE RIGHT
+        nav_dir = '}';
       }
-      else if (courseChangeNeeded >= 255 && courseChangeNeeded < 315) {
-        // TURN LEFT
+      else if (courseChangeNeeded >= 255 && courseChangeNeeded < 315) { // TURN LEFT
+        nav_dir = '<';
       }
-      else if (courseChangeNeeded >= 45 && courseChangeNeeded < 105) {
-        // TURN RIGHT
+      else if (courseChangeNeeded >= 45 && courseChangeNeeded < 105) { // TURN RIGHT
+        nav_dir = '>';
       }
-      else {
-        // TURN AROUND
+      else { // TURN AROUND
+        nav_dir = '&';
       }
     }
 
