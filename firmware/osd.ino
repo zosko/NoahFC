99c204db7df13ce00f6b0fad8b5340b4364b6041
bosko <bosko.petreski@gmail.com>
2022-08-08 18:13:31 +0200

    osd tidy

--- firmware/osd.ino
+++ firmware/osd.ino
@@ -7,10 +7,6 @@ void setupOSD() {
   osd.printMax7456Char(0x01, 0, 1);
   osd.print("Hello world :)", 1, 3);
   osd.print("Current Arduino time :", 1, 4);
-
-  osd.printMax7456Char(0xD1, 9, 6, true);
-  osd.print("00'00\"", 10, 6);
-  osd.printMax7456Chars(tab, 2, 12, 7);
 }
 void showOSD_Data() {
 
