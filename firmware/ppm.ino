532500ba2be0297cbc61c4b7624a140479aa937a
bosko <bosko.petreski@gmail.com>
2022-10-12 11:58:42 +0200

    reduce

--- firmware/ppm.ino
+++
@@ -1,7 +0,0 @@
-void readPPM() {
-  ch1 = ppm.latestValidChannelValue(1, 0);
-  ch2 = ppm.latestValidChannelValue(2, 0);
-  ch3 = ppm.latestValidChannelValue(3, 0);
-  ch4 = ppm.latestValidChannelValue(4, 0);
-  ch5 = ppm.latestValidChannelValue(5, 0);
-}
